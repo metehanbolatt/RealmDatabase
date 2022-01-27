3aff63c13aeb98530b9152171c86a9644bb654a1
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-27 20:38:25 +0300

    splash Screen added. version 1.0

---
+++ app/src/main/java/com/metehanbolat/EntranceActivity.kt
@@ -0,0 +1,32 @@
+package com.metehanbolat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.metehanbolat.realmdatabase.MainActivity
+import com.metehanbolat.realmdatabase.R
+import com.metehanbolat.realmdatabase.databinding.ActivityEntranceBinding
+
+class EntranceActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityEntranceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEntranceBinding.inflate(layoutInflater)
+        val view = binding.root
+        setTheme(R.style.Theme_RealmDatabase)
+        setContentView(view)
+
+        object : CountDownTimer(2500, 1000) {
+            override fun onTick(p0: Long) {}
+            override fun onFinish() {
+                Intent(this@EntranceActivity, MainActivity::class.java).apply {
+                    startActivity(this)
+                    finish()
+                }
+            }
+        }.start()
+    }
+}
